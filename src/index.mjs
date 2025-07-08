5cec2637289c56a11ada38d6311dcf871be42f6a
Helori <helori.lanos@gmail.com>
2025-07-08 15:05:54 +0200

    exports

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,87 @@
-import GenericTable from './Components/GenericTable.vue'
+import useAsset from './Functions/useAsset'
+import useAuth from './Functions/useAuth'
+import useDarkMode from './Functions/useDarkMode'
+import useDebounce from './Functions/useDebounce'
+import useDownloader from './Functions/useDownloader'
+import useList from './Functions/useList'
+import useLocale from './Functions/useLocale'
+import useRequest from './Functions/useRequest'
+import useTable from './Functions/useTable'
+
+import filters from './filters'
+
+import BooleanIcon from './Components/BooleanIcon'
+import Alert from './Components/Alert'
+import DialogForm from './Components/DialogForm'
+import DialogModal from './Components/DialogModal'
+import Drawer from './Components/Drawer'
+import Dropdown from './Components/Dropdown'
+import Collapse from './Components/Collapse'
+import ListFooter from './Components/ListFooter'
+import ListHeader from './Components/ListHeader'
+import ListPagination from './Components/ListPagination'
+import Loader from './Components/Loader'
+import Modal from './Components/Modal'
+import RequestButton from './Components/RequestButton'
+import RequestError from './Components/RequestError'
+import RequestState from './Components/RequestState'
+import RequestStatus from './Components/RequestStatus'
+import TableSortLabel from './Components/TableSortLabel'
+import InputDate from './Components/InputDate'
+import InputFile from './Components/InputFile'
+import InputImage from './Components/InputImage'
+import InputImages from './Components/InputImages'
+import InputMultiselect from './Components/InputMultiselect'
+import LabelValue from './Components/LabelValue'
+import Editor from './Components/Editor'
+import GenericForm from './Components/GenericForm'
+import GenericTable from './Components/GenericTable'
+import GenericSingleton from './Components/GenericSingleton'
+
+let functions = {
+    useAsset,
+    useAuth,
+    useDarkMode,
+    useDebounce,
+    useDownloader,
+    useList,
+    useLocale,
+    useRequest,
+    useTable,
+};
+
+let components = {
+    BooleanIcon,
+    Alert,
+    DialogForm,
+    DialogModal,
+    Drawer,
+    Dropdown,
+    Collapse,
+    ListFooter,
+    ListHeader,
+    ListPagination,
+    Loader,
+    Modal,
+    RequestButton,
+    RequestError,
+    RequestState,
+    RequestStatus,
+    TableSortLabel,
+    InputDate,
+    InputFile,
+    InputImage,
+    InputImages,
+    InputMultiselect,
+    LabelValue,
+    Editor,
+    GenericForm,
+    GenericTable,
+    GenericSingleton,
+};
 
 export {
-    GenericTable,
+    filters,
+    functions,
+    components,
 }
